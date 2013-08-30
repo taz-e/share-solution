e94380c39c69b084fd6e7a687d9b092f1640bf8f
enbake <enbake@enbake.(none)>
2013-08-30 13:55:57 +0530

    #1 Generate Skeleton Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Solution::Application.config.secret_key_base = '0d1e2cb18bc06e689e8a4347c8ef34d8f779c1219876902578c590d03f35b7934c4c4e5624ca2e1dcadc390e829037decece2839e99a476ed7144218b56ad6b3'
